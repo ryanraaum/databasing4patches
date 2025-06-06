bba391de6541f4ebe957db378774f5dbf0f395e9
Ryan Raaum <github@raaum.org>
2025-06-06 12:14:57 -0400

    consolidate testing helpers in testthat directory

--- tests/testthat/helper-table-tests.R
+++ tests/testthat/helper-table-tests.R
@@ -30,33 +30,37 @@ make_testpool <- function(dbtype="sqlite", env=parent.frame()) {
   testcon
 }
 
-# make_testdbobj <- function(dbtype="sqlite", env=parent.frame()) {
-#   if (dbtype == "sqlite") {
-#     dbobj <- ErudicionDB$new(list(drv=RSQLite::SQLite(), dbdir=":memory:"))
-#   } else if (dbtype == "duckdb") {
-#     dbobj <- ErudicionDB$new(list(drv=duckdb::duckdb(), dbdir=":memory:"))
-#   } else {
-#     stop(glue::glue("not a known dbtype: '{dbtype}'"))
-#   }
-#   # withr::defer(
-#   #   dbobj$disconnect(),
-#   #   envir = env
-#   # )
-#   dbobj
-# }
-
-table_columns <- function(con, table_name) {
-  DBI::dbListFields(con, table_name)
+make_testdbobj <- function(dbtype="sqlite", env=parent.frame(), create_tables=FALSE) {
+  if (dbtype == "sqlite") {
+    requireNamespace("RSQLite", quietly = TRUE)
+    dbobj <- DB4Patches$new(list(drv=RSQLite::SQLite(), dbdir=":memory:"),
+                            create_tables=create_tables)
+  } else if (dbtype == "duckdb") {
+    requireNamespace("duckdb", quietly = TRUE)
+    dbobj <- DB4Patches$new(list(drv=duckdb::duckdb(), dbdir=":memory:"),
+                            create_tables=create_tables)
+  } else {
+    stop(glue::glue("not a known dbtype: '{dbtype}'"))
+  }
+  # withr::defer(
+  #   dbobj$disconnect(),
+  #   envir = env
+  # )
+  dbobj
 }
+
+# table_columns <- function(con, table_name) {
+#   DBI::dbListFields(con, table_name)
+# }
 
 supported_databases <- function(just_this_one=NULL) {
   if (is.null(just_this_one)) {return(c("duckdb", "sqlite"))}
   return(just_this_one)
 }
 
-is_sqlite_connection <- function(conn) {
-  if (inherits(conn, "Pool")) {
-    return("SQLiteConnection" %in% conn$objClass)
-  }
-  return(isa(conn, "SQLiteConnection"))
-}
+# is_sqlite_connection <- function(conn) {
+#   if (inherits(conn, "Pool")) {
+#     return("SQLiteConnection" %in% conn$objClass)
+#   }
+#   return(isa(conn, "SQLiteConnection"))
+# }
